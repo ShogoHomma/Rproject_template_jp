1015f95da077100b9f10f7f1a661016dff544f61
ShogoHomma <hom.shogo@gmail.com>
2021-04-02 19:18:35 +0900

    allowed stan_name which doesn't include .stan. added dir_name as a argument

--- Analysis_Script/_functions_Model-BasedAnalysis/output_setup.R
+++ Analysis_Script/_functions_Model-BasedAnalysis/output_setup.R
@@ -1,13 +1,17 @@
-# フィッティング結果の保存先を作成する
+# モデルフィッティングの結果の保存先を作成する
 
-library(tidyverse)
-library(here)
+#library(tidyverse)
+#library(here)
 
-output_setup <- function(output_path, stan_name, fit_number) {
+output_setup <- function(output_path, stan_name, dir_name, fit_number) {
   
-  model_name <- stan_name %>% str_split(pattern = "\\.stan")
+  if (str_detect(stan_name, pattern = "\\.stan") == TRUE) { # .stanが含まれている場合
+    model_name <- stan_name %>% str_split(pattern = "\\.stan") %>% .[[1]] %>% .[1]
+  } else {
+    model_name <- stan_name
+  }
   
-  save_path <- here(output_path, model_name[[1]][1], fit_number, "stanfit")
+  save_path <- here(output_path, model_name, fit_number, dir_name)
   dir.create(save_path, recursive = TRUE)
   
   return(save_path)
