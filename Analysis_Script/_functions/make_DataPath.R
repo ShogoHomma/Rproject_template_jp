cba0ba27ff85d916d4c78284865dc3915cc936f5
ShogoHomma <hom.shogo@gmail.com>
2021-04-02 19:13:02 +0900

    changed file name make_CSVPath -> makeDataPath

--- Analysis_Script/_functions/make_DataPath.R
+++ Analysis_Script/_functions/make_DataPath.R
@@ -1,10 +1,10 @@
-# 複数のcsvファイルのパスを作成する関数
+# 一つ or 複数のデータファイル (.csvなど) のパスを作成する関数
 ## Dir：プロジェクトディレクトリの直下にある、焦点のディレクトリ
 ## Dir_sub：もう1つ下のディレクトリ
 
 library(here)
 
-make_CSVPath <- function(Dir, Dir_sub, dir_pattern, csv_pattern) {
+make_DataPath <- function(Dir, Dir_sub, dir_pattern, csv_pattern) {
   
   target_path <- here(Dir, Dir_sub)
   target_dir <- dir(target_path, recursive = TRUE)[stringr::str_detect(dir(target_path, recursive = TRUE), pattern = dir_pattern)]
