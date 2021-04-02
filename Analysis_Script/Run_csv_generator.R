5643c2181fa41bfe6f40a3e882fd92d3bb4d247c
ShogoHomma <hom.shogo@gmail.com>
2021-04-02 19:21:05 +0900

    functions to make csv file from stan object. Initial Commit

---
+++ Analysis_Script/Run_csv_generator.R
@@ -0,0 +1,35 @@
+# stanをコンパイルし、mcmcを実行するファイル
+
+library(tidyverse)
+library(rstan)
+library(here) 
+
+# 関数の読み込み
+model_functions_path <- here("Analysis_Script", "_functions_Model-BasedAnalysis")
+model_functions <- here(model_functions_path, dir(model_functions_path))
+purrr::walk(model_functions, ~source(.x))
+
+# 結果の出力先
+output_path <- here("Data", "Models_Results")
+
+
+# ------ basic_RL model ----------
+
+# * fit01 -------
+
+stan_name <- "basic_RL"
+fit_number <- "fit01"
+stanfit_pattern <- "stanfit.obj"
+output_csv_name <- "est_parameter.csv"
+
+# ファイルの保存先を作成 & 取得
+save_path <- output_setup(output_path, stan_name, "estimated_parameter", fit_number) 
+
+# stan.objの取得
+target_stanfit <- make_DataPath(output_path, "basic_RL", "fit01", stanfit_pattern)
+
+# csvの作成
+params_csv_basicRL(target_stanfit, save_path, output_csv_name)
+
+
+
