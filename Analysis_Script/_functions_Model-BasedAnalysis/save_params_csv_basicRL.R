05e899b6faea0722d18bf497f2edb5635e0cbd5b
shogohomma <hom.shogo@gmail.com>
2021-04-06 16:15:56 +0900

    changed purrr::map_dfr() -> stan_to_df, ommited the case multiple stan.obj

--- Analysis_Script/_functions_Model-BasedAnalysis/save_params_csv_basicRL.R
+++ Analysis_Script/_functions_Model-BasedAnalysis/save_params_csv_basicRL.R
@@ -5,8 +5,7 @@
 
 params_csv_basicRL <- function(target_stanfit, save_path, output_csv_name) {
   
-  # stan.objが複数ある可能性も考えて、purrr::map_dfr()で1つに結合しながら読み込む
-  stan_df <- purrr::map_dfr(target_stanfit, stan_to_df) %>% 
+  stan_df <- stan_to_df(target_stanfit) %>% 
     as_tibble() # 一応tibble化しておく
   
   save_csv_path <- paste0(save_path, "/", output_csv_name)
