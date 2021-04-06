161e8c8aca4bec2dd9b29cde59548214389a5ca1
Shogo Homma <shogohomma@Shogos-iMac.local>
2021-04-06 15:04:32 +0900

    change params_csv_basicRL -> save_params_...

--- Analysis_Script/Run_csv_generator.R
+++ Analysis_Script/Run_csv_generator.R
@@ -9,6 +9,10 @@ model_functions_path <- here("Analysis_Script", "_functions_Model-BasedAnalysis"
 model_functions <- here(model_functions_path, dir(model_functions_path))
 purrr::walk(model_functions, ~source(.x))
 
+common_functions_path <- here("Analysis_Script", "_functions")
+common_functions <- here(common_functions_path, dir(common_functions_path))
+purrr::walk(common_functions, ~source(.x))
+
 # 結果の出力先
 output_path <- here("Data", "Models_Results")
 
@@ -25,11 +29,12 @@ output_csv_name <- "est_parameter.csv"
 # ファイルの保存先を作成 & 取得
 save_path <- output_setup(output_path, stan_name, "estimated_parameter", fit_number) 
 
-# stan.objの取得
+# stan.objのパスを取得
 target_stanfit <- make_DataPath(output_path, "basic_RL", "fit01", stanfit_pattern)
 
 # csvの作成
-params_csv_basicRL(target_stanfit, save_path, output_csv_name)
+save_params_csv_basicRL(target_stanfit, save_path, output_csv_name)
+
 
 
 
