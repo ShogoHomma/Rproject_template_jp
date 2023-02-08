064fe619ace5f1a35911c56dc598e85222db05f7
ShogoHomma <hom.shogo@gmail.com>
2023-02-08 18:07:30 +0900

    updated the directory structure & file name

--- Analysis_Script/_functions_common/%&%.R
+++
@@ -1,8 +0,0 @@
-# 文字列を結合する演算子: pythonみたいな結合ができるように
-# ideas from Nishiura sensei
-
-`%&%` <- function(s1, s2) {
-  
-  paste0(s1, s2)
-  
-}
--- Analysis_Script/_functions_common/map_value.R
+++
@@ -1,8 +0,0 @@
-# MAP (maximum a posteriori) 値を返す関数
-
-map_value <- function(z) {
-  
-  val <- density(z)$x[which.max(density(z)$y)]
-  return(val)
-  
-}
